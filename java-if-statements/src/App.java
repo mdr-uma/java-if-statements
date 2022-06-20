ea95cdab9666091871b7e58f55f0d8e8065a6110
github username <um_2003_8@hotmail.com>
2022-06-19 20:55:03 -0400

    add if else statement

--- java-if-statements/src/App.java
+++ java-if-statements/src/App.java
@@ -1,9 +1,18 @@
 
 public class App {
 	public static void main(String[] args) {
+		int num = 40;
+		
 		//simple if statement where if condition meets print something otherwise nothing to print
 		if(5 > 4) {
 			System.out.println("It's true");
+		}
+		
+		//if statement with else where, if the condition is true print something otherwise print the else statement		
+		if(num == 30) {
+			System.out.println("It's true");
+		}else{
+			System.out.println("It's False");
 		}
 	}
 
